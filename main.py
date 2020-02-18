5cdba62a5be4ae9c15877f53c57dd18a2a1b8b99
Gogichagiorgi <gogichaishvilig544@example.com>
2020-02-18 11:19:23 +0400

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello world")
+import random
+import time
+
+roll_again = "yes"
+while roll_again == "yes" or roll_again == 'y' or roll_again == "yes" or roll_again == "y":
+        print("\nRolling the dice...")
+        time.sleep(1)
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+    
+        print("The values are:")
+        print("Dice 1 =", dice1, "\nDice 2 =", dice2)
+ 
+        if dice1 == dice2:
+            print("tqven moiget!")
+        if dice1 and dice2 <1 or dice1 and dice2 > 6:
+            print("araswori ricxvia!")
+        else:
+                print("kidev scadet")
+        roll_again = input("\Roll the dice again ? (Y/N) ")
+
+
+            
